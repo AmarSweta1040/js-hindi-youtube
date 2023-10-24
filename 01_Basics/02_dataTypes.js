4a72f682111789f679c58ae237fa86b981f5718a
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-24 14:45:55 +0000

    Commit

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -1,26 +1,7 @@
 "use trict"; //treat all JS code as newer version
-
+//alert(3+3)  we are using nodejs, not browser
 //alert("Hello")// we are using nodejs, not browser
 
-console.log(3+3)
-
-console.log("Amar")
-
-
-let name = "Amar"//string
-let age = 18//integer
-let isLoggedIn = false  //boolen
-
-//number => 2 to the power 53
-//bigint
-//string
-//boolean => true / false
-//null => standalone value
-//undefined => 
-//symbol => mostly used for uniquenesess
-
-//object
-
-console.log(typeof name)
-console.log(typeof undefined) //undefined
-console.log(typeof null) //object
+let name = " Amar "  //string
+let age = 18  // integer
+let isLogin = false // boolen
