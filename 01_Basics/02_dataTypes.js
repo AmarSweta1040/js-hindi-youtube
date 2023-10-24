7b6a2804a30a277371567f390a7d37fa935c1402
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-24 15:15:18 +0000

    Commit

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -4,4 +4,22 @@
 
 let name = " Amar "  //string
 let age = 18  // integer
-let isLogin = false // boolen
+let isLoggedIn = false // boolen
+let hello = null
+let state; // undefined
+
+
+// number => 2 to the power 53
+// bigint
+// string ==> ""
+// boolean ==> True / False
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
