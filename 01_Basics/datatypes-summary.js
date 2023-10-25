6f104756fd64dea9ff123e5717256eb1752ea185
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-25 03:21:56 +0000

    Commit

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive Data Type
+/*Total 7 Types:
+1. String
+2. Number
+3. Boolean
+4. null
+5. undefined
+6. Symbol
+7. BigInt
+*/
+
+/*
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, 
+and you do not need to explicitly declare the type of a variable before using it. You can 
+assign different types of values to a variable during its lifetime.
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type 
+explicitly, and the type checking is done at compile-time, before the code is executed.
+
+Languages like Java, C++, and TypeScript are statically typed, and they require you to 
+specify the variable type explicitly when declaring them:
+
+int x = 10; // x is a variable of type int
+String name = "John"; // name is a variable of type String
+JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime 
+errors if not handled carefully. Static typing, on the other hand, provides better type safety at 
+the cost of some initial verbosity and strictness.
+*/
+/* Non Primitive Data Type / Reference Data Type
+1. Array
+2. Objects
+3. Functions*/
