eecf7cd061611d13e404e760f613d6fbf1b1e890
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-25 02:51:24 +0000

    Commit

--- 01_Basics/03_conversionOperation2.js
+++ 01_Basics/03_conversionOperation2.js
@@ -16,4 +16,21 @@ let str2 = " Amar"
 let str3 = str1 + str2
 console.log(str3) // Hello Amar
 
-hh
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+/*Javascript में पहले अगर number है तो तो वह पहले number की तरह काम करेगा*/
+/*अगर पहले string होगा तो पूरा concatenation हो जाएगा*/
+
+console.log(true)  // output=> true
+console.log(+true) // output=> 1
+console.log(+"")   // output=> 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.table([num1, num2, num3])
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+//Confusing topic started
+console.log("2" > 1)
+console.log("02" > 1)
