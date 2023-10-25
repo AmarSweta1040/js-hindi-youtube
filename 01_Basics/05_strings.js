5e910afd60225328c0eef0a138a262d03552c82c
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-25 14:30:08 +0000

    Commit

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,4 +1,17 @@
 const name = "Amar"
 const repoCount = 50
 
-console.log(name + repoCount + " Value")
+//console.log(name + repoCount + " Value") don't use
+
+console.log(`Hello mu name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Hello")
+
+console.log(gameName[0]) // h
+console.log(gameName.length) // f
+console.log(gameName.toUpperCase()) // HELLO
+console.log(gameName.charAt(3)) //l
+console.log(gameName.indexOf('o')) //4
+
+const newString = new String("HelloWorld")
+console.log(newString.substring(0,4))
