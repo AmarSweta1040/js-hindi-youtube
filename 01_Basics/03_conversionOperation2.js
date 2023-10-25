b5fb325853a70b2df302857eac5f88f18efbebb2
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-25 01:46:02 +0000

    Commit

---
+++ 01_Basics/03_conversionOperation2.js
@@ -0,0 +1,18 @@
+/*********** Operations ***********/
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2) //4
+console.log(2-2) //0
+console.log(2*2) //4
+console.log(2/2) //1
+console.log(2%2) //0
+console.log(2**3)//8
+
+let str1 = "Hello"
+let str2 = " Amar"
+let str3 = str1 + str2
+console.log(str3) // Hello Amar
+
