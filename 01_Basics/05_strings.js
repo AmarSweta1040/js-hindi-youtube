7cab9daf5985ad88da0d26dc77b29b2aaa19fd1d
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-26 06:28:22 +0000

    comitting

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -5,13 +5,26 @@ const repoCount = 50
 
 console.log(`Hello mu name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String("Hello")
+const gameName = new String("Hello-hc")
 
-console.log(gameName[0]) // h
-console.log(gameName.length) // f
-console.log(gameName.toUpperCase()) // HELLO
+console.log(gameName[0]) // H
+console.log(gameName.length) // 8
+console.log(gameName.toUpperCase()) // HELLO-HC
 console.log(gameName.charAt(3)) //l
 console.log(gameName.indexOf('o')) //4
 
 const newString = new String("HelloWorld")
-console.log(newString.substring(0,4))
+console.log(newString.substring(0,4))//Hell
+
+const anotherString = newString.slice(-8,4)
+console.log(anotherString)
+
+const newStringOne = "    Hello   "
+console.log(newStringOne);
+console.log(newStringOne.trim());//Used to remove spaces
+
+const url = "https://amar.com/amar%20maurya"
+console.log(url.replace('%20','-')) // https://amar.com/amar-maurya
+console.log(url.includes("amar")) //true
+
+console.log(gameName.split('-'))
