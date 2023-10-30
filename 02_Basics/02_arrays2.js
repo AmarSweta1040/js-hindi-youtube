19a3a5630bb04af9700125e8a8b780e9f0ed7eb7
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-10-30 03:39:10 +0000

    Commit

---
+++ 02_Basics/02_arrays2.js
@@ -0,0 +1,8 @@
+const marvel_heros = ["thor", "Iron Man", "Spider Man",5]
+const dc_heros = ["superman", "Batman", "Flash"]
+
+marvel_heros.push(dc_heros);
+//console.log(marvel_heros);
+
+dc_heros.concat(dc_heros);
+console.log(dc_heros);
