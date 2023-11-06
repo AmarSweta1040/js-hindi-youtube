1a3a12c54deef90e37d71704f1db289f70e479e2
AmarSweta1040 <113175179+AmarSweta1040@users.noreply.github.com>
2023-11-06 02:59:56 +0000

    Commit

---
+++ 02_Basics/05_objects3.js
@@ -0,0 +1,13 @@
+const course = {
+    courseName : "Js in Hindi",
+    price : 999,
+    courseInstructor : "Amar"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : abcxyz} = course
+
+//console.log(courseInstructor);
+
+console.log(abcxyz);
