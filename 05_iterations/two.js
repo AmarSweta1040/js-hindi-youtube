0c211af9e4cced1e14f7a247096fe7e041bc89fa
Amar Maurya <113175179+AmarSweta1040@users.noreply.github.com>
2023-11-27 06:13:10 +0000

    commit

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let i = 1
+while ( i <= 5) {
+    console.log(i);
+    i++;
+} // 1 2 3 4 5
+
+
+let myArray = ["Flash", "Batman", "Supermen"]
+
+let arr = 0
+while (arr<myArray.length) {
+    console.log(myArray[arr])
+    arr++;
+}
+
+
+
+// do while loop
+ let score = 11
+do{
+    console.log(score);
+    score++
+} while (score <= 10)
